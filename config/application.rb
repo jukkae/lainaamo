706788c25e8c934b2e58283c61584b616b4594ca
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2016-03-06 14:46:32 +0200

    use devise

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Lainaamo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Do not access DB when precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
